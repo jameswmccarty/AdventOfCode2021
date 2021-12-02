71af7fe0616c0b6681386dafcf60827cefbca394
James McCarty <jameswmccarty@gmail.com>
2021-12-02 18:25:41 -0500

    Cleanup

--- day02.py
+++ day02.py
@@ -77,34 +77,32 @@ if __name__ == "__main__":
 
 	with open("day02_input","r") as infile:
 		orders = infile.read().strip().split('\n')
-		horiz = 0
-		up    = 0
-		down  = 0
-		for order in orders:
-			command, unit = order.split(' ')
-			if command == 'forward':
-				horiz += int(unit)
-			elif command == 'up':
-				up += int(unit)
-			elif command == 'down':
-				down += int(unit)
-		print(horiz * (down-up))
+		orders = [ x.split(' ') for x in orders ]
+		orders = [ (x[0],int(x[1])) for x in orders ]
+	horiz = 0
+	up    = 0
+	down  = 0
+	for command, unit in orders:
+		if command == 'forward':
+			horiz += unit
+		elif command == 'up':
+			up += unit
+		elif command == 'down':
+			down += unit
+	print(horiz * (down-up))
 
 
 	# Part 2 Solution
 
-	with open("day02_input","r") as infile:
-		orders = infile.read().strip().split('\n')
-		horiz = 0
-		aim   = 0
-		depth = 0
-		for order in orders:
-			command, unit = order.split(' ')
-			if command == 'forward':
-				horiz += int(unit)
-				depth += int(unit)*aim
-			elif command == 'up':
-				aim -= int(unit)
-			elif command == 'down':
-				aim += int(unit)
-		print(horiz * depth)
+	horiz = 0
+	aim   = 0
+	depth = 0
+	for command, unit in orders:
+		if command == 'forward':
+			horiz += unit
+			depth += unit*aim
+		elif command == 'up':
+			aim -= unit
+		elif command == 'down':
+			aim += unit
+	print(horiz * depth)
