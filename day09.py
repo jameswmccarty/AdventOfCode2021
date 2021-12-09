b2f42f57ac31ceac38b3baf8fb7ea70a4d58df0e
jameswmccarty <jameswmccarty@gmail.com>
2021-12-09 14:47:40 -0500

    Update for a Part 2 solution

--- day09.py
+++ day09.py
@@ -73,16 +73,19 @@ Find the three largest basins and multiply their sizes together. In the above ex
 
 What do you get if you multiply together the sizes of the three largest basins?
 
+Your puzzle answer was 1558722.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 if __name__ == "__main__":
 
-	# Part 1 Solution (1654 too high)
+	# Part 1 Solution
 
 	adj_points = [(1,0),(-1,0),(0,1),(0,-1)]
-
 	map_points = dict()
-
+	low_points = []
 	with open("day09_input","r") as infile:
 		cavemap = infile.read().strip()
 	cavemap = cavemap.split('\n')
@@ -97,7 +100,33 @@ if __name__ == "__main__":
 				lowest = False
 		if lowest:
 			low_total += (map_points[(a,b)]+1)
+			low_points.append((a,b))
 	print(low_total)
 
 
 	# Part 2 Solution
+	basins = []
+	visited = set()
+	for x,y in low_points:
+		if (x,y) not in visited:
+			basin = set()
+			basin.add((x,y))
+			q = [(x,y)]
+			while len(q) > 0:
+				x,y = q.pop(0)
+				visited.add((x,y))
+				for dx,dy in adj_points:
+					if (x+dx,y+dy) in visited:
+						for b in basins:
+							if (x+dx,y+dy) in b:
+								basin = basin + b
+								b = set()
+					elif (x+dx,y+dy) in map_points and map_points[(x+dx,y+dy)] != 9:
+						q.append((x+dx,y+dy))
+						basin.add((x+dx,y+dy))
+			basins.append(basin)
+	sizes = sorted([ len(x) for x in basins if len(x) > 0 ], reverse = True)
+	print(sizes[0]*sizes[1]*sizes[2])
+
+		
+	
