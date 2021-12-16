916fce8744bddabb12e2a4e13ceef566a2ff0ba8
James McCarty <jameswmccarty@gmail.com>
2021-12-15 19:29:40 -0500

    Slight speed improvement

--- day15.py
+++ day15.py
@@ -177,8 +177,7 @@ if __name__ == "__main__":
 	cave_map = dict()
 	cave_dim = None
 
-	def cost_at(pos):
-		x,y = pos
+	def cost_at(x,y):
 		risk = cave_map[x%cave_dim,y%cave_dim] + x//cave_dim + y//cave_dim
 		return risk%10+1 if risk > 9 else risk
 
@@ -186,16 +185,15 @@ if __name__ == "__main__":
 		q = []
 		heapq.heapify(q)
 		seen = set((0,0))
-		heapq.heappush(q,(0,(0,0)))
+		heapq.heappush(q,(0,0,0))
 		while len(q) > 0:
-			risk,pos = heapq.heappop(q)
-			x,y = pos
-			if pos == goal:
+			risk,x,y = heapq.heappop(q)
+			if (x,y) == goal:
 				return risk
 			for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
 				nx,ny = x+dx, y+dy
 				if (nx,ny) not in seen and nx >= 0 and nx < cave_dim*scale and ny >= 0 and ny < cave_dim*scale:
-					heapq.heappush(q,(risk+cost_at((nx,ny)),(nx,ny)))
+					heapq.heappush(q,(risk+cost_at(nx,ny),nx,ny))
 					seen.add((nx,ny))
 		return float('inf')
 
