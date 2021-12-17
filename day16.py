74451e547f94583fea8c1b98a06a3d489ef63045
James McCarty <jameswmccarty@gmail.com>
2021-12-16 19:35:33 -0500

    Update for part 2

--- day16.py
+++ day16.py
@@ -128,6 +128,10 @@ For example:
 
 What do you get if you evaluate the expression represented by your hexadecimal-encoded BITS transmission?
 
+Your puzzle answer was 1725277876501.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 if __name__ == "__main__":
@@ -177,16 +181,13 @@ if __name__ == "__main__":
 		return out
 
 	def parse_packets(packet):
-		print("Parsing packet",hex(packet))
 		global version_count
 		bits_processed = 0
 		V = read_n_bin(3,packet)
 		version_count += V
 		packet = packet >> 3
 		bits_processed += 3
-		print("version",V)
 		T = read_n_bin(3,packet)
-		print("type", T)
 		packet = packet >> 3
 		bits_processed += 3
 		if T == 4: # found literal value
@@ -200,8 +201,6 @@ if __name__ == "__main__":
 				L += read_n_str(4,packet)
 				packet = packet >> 4
 				bits_processed += 4
-			print("value",int(L,2))
-			print("processed",bits_processed)
 			return bits_processed
 		else: # found an operator
 			length_type_id = read_n_bin(1,packet)
@@ -211,7 +210,6 @@ if __name__ == "__main__":
 				length = read_n_bin(15,packet)
 				packet = packet >> 15
 				bits_processed += 15
-				print("Type 1 bits to process",length)
 				segment = read_n_str(length,packet)
 				packet = packet >> length
 				bits_processed += length
@@ -224,18 +222,84 @@ if __name__ == "__main__":
 					segment = segment >> round_total
 			elif length_type_id == 1:
 				num_packets = read_n_bin(11,packet)
-				print("Type 0 number of packets",num_packets)
 				packet = packet >> 11
 				bits_processed += 11
 				for i in range(num_packets):
 					processed = parse_packets(packet)
 					packet = packet >> processed
 					bits_processed += processed
-		print("processed",bits_processed)
 		return bits_processed
 
+	def compute_packets(packet):
+		bits_processed = 0
+		V = read_n_bin(3,packet)
+		packet = packet >> 3
+		bits_processed += 3
+		T = read_n_bin(3,packet)
+		packet = packet >> 3
+		bits_processed += 3
+		if T == 4: # found literal value
+			end_found = False
+			L = ''
+			while not end_found:
+				if read_n_bin(1,packet) != 1:
+					end_found = True
+				packet = packet >> 1
+				bits_processed += 1
+				L += read_n_str(4,packet)
+				packet = packet >> 4
+				bits_processed += 4
+			return (bits_processed,int(L,2))
+		else: # found an operator
+			length_type_id = read_n_bin(1,packet)
+			packet = packet >> 1
+			bits_processed += 1
+			sub_values = []
+			if length_type_id == 0:
+				length = read_n_bin(15,packet)
+				packet = packet >> 15
+				bits_processed += 15
+				segment = read_n_str(length,packet)
+				packet = packet >> length
+				bits_processed += length
+				segment = segment[::-1]
+				segment = int(segment,2)
+				sub_seg_total = 0
+				while sub_seg_total < length:
+					round_total, sub_val = compute_packets(segment)
+					sub_seg_total += round_total
+					segment = segment >> round_total
+					sub_values.append(sub_val)
+			elif length_type_id == 1:
+				num_packets = read_n_bin(11,packet)
+				packet = packet >> 11
+				bits_processed += 11
+				for i in range(num_packets):
+					processed, sub_val = compute_packets(packet)
+					packet = packet >> processed
+					bits_processed += processed
+					sub_values.append(sub_val)
+			if T == 0:
+				return (bits_processed, sum(sub_values))
+			if T == 1:
+				total = 1
+				for val in sub_values:
+					total *= val
+				return (bits_processed, total)
+			if T == 2:
+				return (bits_processed, min(sub_values))
+			if T == 3:
+				return (bits_processed, max(sub_values))
+			if T == 5:
+				return (bits_processed, 1 if sub_values[0] > sub_values[1] else 0)
+			if T == 6:
+				return (bits_processed, 1 if sub_values[0] < sub_values[1] else 0)
+			if T == 7:
+				return (bits_processed, 1 if sub_values[0] == sub_values[1] else 0)
+
+
 	# Part 1 Solution
-	packet = "A0016C880162017C3686B18A3D4780"
+	packet = "04005AC33890"
 	with open("day16_input","r") as infile:
 		packet = infile.read().strip()
 	initial_len = len(packet)
@@ -244,9 +308,9 @@ if __name__ == "__main__":
 	stream = int(packet,16) & int('F'*initial_len,16)
 
 	parse_packets(stream)
-	print("Version sum", version_count)
+	print(version_count)
 
 	# Part 2 Solution
-
+	print(compute_packets(stream)[1])
 
 
