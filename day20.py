42e56357cc0b70b34dfe5788cf45f9e5ef635b4b
jameswmccarty <jameswmccarty@gmail.com>
2021-12-20 18:50:11 -0500

    Cleanup and speed improvements

--- day20.py
+++ day20.py
@@ -134,29 +134,17 @@ if __name__ == "__main__":
 	y_min = None
 	y_max = None
 	
-	def find_bounds():
-		global x_min,x_max,y_min,y_max
-		x_min = float('inf')
-		x_max = -float('inf')
-		y_min = float('inf')
-		y_max = -float('inf')
-		for x,y in lit:
-			x_min = min(x_min,x)
-			x_max = max(x_max,x)
-			y_min = min(y_min,y)
-			y_max = max(y_max,y)
-		
 	def resolve_px(x,y,t):
-		out = ''
+		out = 0
+		idx = 8
 		for j in (-1,0,1):
 			for i in (-1,0,1):
 				if (x+i,y+j) in lit:
-					out += '1'
-				elif (x+i < x_min or x+i > x_max or y+j < y_min or y+j > y_max) and t%2 == 1:
-					out += '1'
-				else:
-					out += '0'
-		return int(out,2)
+					out |= 1 << idx
+				elif t%2 == 1 and  (x+i < x_min or x+i > x_max or y+j < y_min or y+j > y_max):
+					out |= 1 << idx
+				idx -= 1
+		return out
 
 	# Part 1 Solution
 	
@@ -165,57 +153,53 @@ if __name__ == "__main__":
 		throw  = infile.readline()
 		y = 0
 		for line in infile.readlines():
+			x_max = len(line)
 			for x,char in enumerate(line.strip()):
 				if char == "#":
 					lit.add((x,y))
 			y += 1
+		x_min = 0
+		y_min = 0
+		y_max = y		
 	
-	find_bounds()
-	orig_x_min = x_min
+	orig_x_min = x_min # save initial state
 	orig_x_max = x_max
 	orig_y_min = y_min
 	orig_y_max = y_max
+	orig_points = lit
 
 	for _ in range(2):
 		next_lit = set()
-		find_bounds()
-		for y in range(y_min-4,y_max+4):
-			for x in range(x_min-4,x_max+4):
+		for y in range(y_min-1,y_max+2):
+			for x in range(x_min-1,x_max+2):
 				if kernel[resolve_px(x,y,_)] == "#":
 					next_lit.add((x,y))
 		lit = next_lit
-	lit = { x for x in lit if x[0] >= orig_x_min-2 and x[0] <= orig_x_max +2 }
-	lit = { x for x in lit if x[1] >= orig_y_min-2 and x[1] <= orig_y_max +2 }
+		y_min -= 1
+		x_min -= 1
+		x_max += 1
+		y_max += 1
 	print(len(lit))
 
 	# Part 2 Solution
 
-	lit = set()
-	with open("day20_input","r") as infile:
-		kernel = infile.readline().strip()
-		throw  = infile.readline()
-		y = 0
-		for line in infile.readlines():
-			for x,char in enumerate(line.strip()):
-				if char == "#":
-					lit.add((x,y))
-			y += 1
-	
-	find_bounds()
-	orig_x_min = x_min
-	orig_x_max = x_max
-	orig_y_min = y_min
-	orig_y_max = y_max
+	#reset
+	lit = orig_points
+	x_min = orig_x_min
+	x_max = orig_x_max
+	y_min = orig_y_min
+	y_max = orig_y_max
 
 	for _ in range(50):
 		next_lit = set()
-		find_bounds()
-		for y in range(y_min-3,y_max+3):
-			for x in range(x_min-3,x_max+3):
+		for y in range(y_min-1,y_max+2):
+			for x in range(x_min-1,x_max+2):
 				if kernel[resolve_px(x,y,_)] == "#":
 					next_lit.add((x,y))
 		lit = next_lit
-	lit = { x for x in lit if x[0] >= orig_x_min-50 and x[0] <= orig_x_max +50 }
-	lit = { x for x in lit if x[1] >= orig_y_min-50 and x[1] <= orig_y_max +50 }
+		y_min -= 1
+		x_min -= 1
+		x_max += 1
+		y_max += 1
 	print(len(lit))
 
