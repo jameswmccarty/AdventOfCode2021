bb954b3b86b6670635daf3cea2ccadeeb5b94780
James McCarty <jameswmccarty@gmail.com>
2021-12-31 20:26:50 -0500

    Add Day 18 files (Part 1)

---
+++ day18.py
@@ -0,0 +1,293 @@
+#!/usr/bin/python
+
+"""
+
+--- Day 18: Snailfish ---
+
+You descend into the ocean trench and encounter some snailfish. They say they saw the sleigh keys! They'll even tell you which direction the keys went if you help one of the smaller snailfish with his math homework.
+
+Snailfish numbers aren't like regular numbers. Instead, every snailfish number is a pair - an ordered list of two elements. Each element of the pair can be either a regular number or another pair.
+
+Pairs are written as [x,y], where x and y are the elements within the pair. Here are some example snailfish numbers, one snailfish number per line:
+
+[1,2]
+[[1,2],3]
+[9,[8,7]]
+[[1,9],[8,5]]
+[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
+[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
+[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]
+
+This snailfish homework is about addition. To add two snailfish numbers, form a pair from the left and right parameters of the addition operator. For example, [1,2] + [[3,4],5] becomes [[1,2],[[3,4],5]].
+
+There's only one problem: snailfish numbers must always be reduced, and the process of adding two snailfish numbers can result in snailfish numbers that need to be reduced.
+
+To reduce a snailfish number, you must repeatedly do the first action in this list that applies to the snailfish number:
+
+    If any pair is nested inside four pairs, the leftmost such pair explodes.
+    If any regular number is 10 or greater, the leftmost such regular number splits.
+
+Once no action in the above list applies, the snailfish number is reduced.
+
+During reduction, at most one action applies, after which the process returns to the top of the list of actions. For example, if split produces a pair that meets the explode criteria, that pair explodes before other splits occur.
+
+To explode a pair, the pair's left value is added to the first regular number to the left of the exploding pair (if any), and the pair's right value is added to the first regular number to the right of the exploding pair (if any). Exploding pairs will always consist of two regular numbers. Then, the entire exploding pair is replaced with the regular number 0.
+
+Here are some examples of a single explode action:
+
+    [[[[[9,8],1],2],3],4] becomes [[[[0,9],2],3],4] (the 9 has no regular number to its left, so it is not added to any regular number).
+    [7,[6,[5,[4,[3,2]]]]] becomes [7,[6,[5,[7,0]]]] (the 2 has no regular number to its right, and so it is not added to any regular number).
+    [[6,[5,[4,[3,2]]]],1] becomes [[6,[5,[7,0]]],3].
+    [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]] becomes [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]] (the pair [3,2] is unaffected because the pair [7,3] is further to the left; [3,2] would explode on the next action).
+    [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]] becomes [[3,[2,[8,0]]],[9,[5,[7,0]]]].
+
+To split a regular number, replace it with a pair; the left element of the pair should be the regular number divided by two and rounded down, while the right element of the pair should be the regular number divided by two and rounded up. For example, 10 becomes [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
+
+Here is the process of finding the reduced result of [[[[4,3],4],4],[7,[[8,4],9]]] + [1,1]:
+
+after addition: [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]
+after explode:  [[[[0,7],4],[7,[[8,4],9]]],[1,1]]
+after explode:  [[[[0,7],4],[15,[0,13]]],[1,1]]
+after split:    [[[[0,7],4],[[7,8],[0,13]]],[1,1]]
+after split:    [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]
+after explode:  [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
+
+Once no reduce actions apply, the snailfish number that remains is the actual result of the addition operation: [[[[0,7],4],[[7,8],[6,0]]],[8,1]].
+
+The homework assignment involves adding up a list of snailfish numbers (your puzzle input). The snailfish numbers are each listed on a separate line. Add the first snailfish number and the second, then add that result and the third, then add that result and the fourth, and so on until all numbers in the list have been used once.
+
+For example, the final sum of this list is [[[[1,1],[2,2]],[3,3]],[4,4]]:
+
+[1,1]
+[2,2]
+[3,3]
+[4,4]
+
+The final sum of this list is [[[[3,0],[5,3]],[4,4]],[5,5]]:
+
+[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+
+The final sum of this list is [[[[5,0],[7,4]],[5,5]],[6,6]]:
+
+[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]
+
+Here's a slightly larger example:
+
+[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]
+
+The final sum [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]] is found after adding up the above snailfish numbers:
+
+  [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
++ [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+= [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
+
+  [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
++ [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+= [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]
+
+  [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]
++ [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+= [[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]
+
+  [[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]
++ [7,[5,[[3,8],[1,4]]]]
+= [[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]
+
+  [[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]
++ [[2,[2,2]],[8,[8,1]]]
+= [[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]
+
+  [[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]
++ [2,9]
+= [[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]
+
+  [[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]
++ [1,[[[9,3],9],[[9,0],[0,7]]]]
+= [[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]
+
+  [[[[7,8],[6,7]],[[6,8],[0,8]]],[[[7,7],[5,0]],[[5,5],[5,6]]]]
++ [[[5,[7,4]],7],1]
+= [[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]
+
+  [[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]
++ [[[[4,2],2],6],[8,7]]
+= [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]
+
+To check whether it's the right answer, the snailfish teacher only checks the magnitude of the final sum. The magnitude of a pair is 3 times the magnitude of its left element plus 2 times the magnitude of its right element. The magnitude of a regular number is just that number.
+
+For example, the magnitude of [9,1] is 3*9 + 2*1 = 29; the magnitude of [1,9] is 3*1 + 2*9 = 21. Magnitude calculations are recursive: the magnitude of [[9,1],[1,9]] is 3*29 + 2*21 = 129.
+
+Here are a few more magnitude examples:
+
+    [[1,2],[[3,4],5]] becomes 143.
+    [[[[0,7],4],[[7,8],[6,0]]],[8,1]] becomes 1384.
+    [[[[1,1],[2,2]],[3,3]],[4,4]] becomes 445.
+    [[[[3,0],[5,3]],[4,4]],[5,5]] becomes 791.
+    [[[[5,0],[7,4]],[5,5]],[6,6]] becomes 1137.
+    [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]] becomes 3488.
+
+So, given this example homework assignment:
+
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+
+The final sum is:
+
+[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]
+
+The magnitude of this final sum is 4140.
+
+Add up all of the snailfish numbers from the homework assignment in the order they appear. What is the magnitude of the final sum?
+
+Your puzzle answer was 3654.
+
+The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+You notice a second question on the back of the homework assignment:
+
+What is the largest magnitude you can get from adding only two of the snailfish numbers?
+
+Note that snailfish addition is not commutative - that is, x + y and y + x can produce different results.
+
+Again considering the last example homework assignment above:
+
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+
+The largest magnitude of the sum of any two snailfish numbers in this list is 3993. This is the magnitude of [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]] + [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]], which reduces to [[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]].
+
+What is the largest magnitude of any sum of two different snailfish numbers from the homework assignment?
+
+"""
+
+import math
+
+def concat_statements(s1,s2):
+	return ['['] + s1 + s2 + [']']
+
+def build_statement(string):
+	statement = []
+	for char in string:
+		if char in "0123456789":
+			statement.append(int(char))
+		elif char in "[]":
+			statement.append(char)
+	return statement
+
+def find_explodable_index(statement):
+	l_count = 0
+	for idx,char in enumerate(statement):
+		if char == '[':
+			l_count += 1
+		elif char == ']':
+			l_count -= 1
+		if l_count >= 5 and isinstance(statement[idx+1],int):
+			return idx+1
+	return -1
+
+def explode_at_index(statement,idx):
+	l_value = statement[idx]
+	r_value = statement[idx+1]
+	out_statement = statement[0:idx-1] + [0] + statement[idx+3:]
+	for l_idx in range(idx-2,-1,-1):
+		if isinstance(out_statement[l_idx],int):
+			out_statement[l_idx] += l_value
+			break
+	for r_idx in range(idx,len(out_statement)):
+		if isinstance(out_statement[r_idx],int):
+			out_statement[r_idx] += r_value
+			break
+	return out_statement
+
+def find_splitable_index(statement):
+	for idx,char in enumerate(statement):
+		if isinstance(char,int) and char >= 10:
+			return idx
+	return -1
+
+def split_at_index(statement,idx):
+	l_value = statement[idx]//2
+	r_value = math.ceil(statement[idx]/2)
+	return statement[0:idx] + ['[',l_value,r_value,']'] + statement[idx+1:]
+
+def solve_statement(statement):
+	solved = False
+	while not solved:
+		solved = True
+		idx = find_explodable_index(statement)
+		if idx > -1:
+			solved = False
+			statement = explode_at_index(statement,idx)
+		else:
+			while True:
+				idx = find_splitable_index(statement)
+				if idx > -1:
+					solved = False
+					statement = split_at_index(statement,idx)
+					break
+				else:
+					break
+	return statement
+
+def find_magnitude(s):
+	while len(s) > 3:
+		for idx in range(len(s)-3):
+			if s[idx] == '[' and isinstance(s[idx+1],int) and isinstance(s[idx+2],int) and s[idx+3] == ']':
+				new_val = s[idx+1]*3 + s[idx+2]*2
+				s = s[0:idx] + [new_val] + s[idx+4:]
+				break
+	return s[0]
+
+if __name__ == "__main__":
+
+	# Part 1 Solution
+	lines = []
+	with open("day18_input","r") as infile:
+		for line in infile.readlines():
+			lines.append(build_statement(line.strip()))
+	working_statement = lines.pop(0)
+
+	while len(lines) > 0:
+		working_statement = concat_statements(working_statement,lines[0])
+		lines = lines[1:]
+		working_statement = solve_statement(working_statement)
+	print(find_magnitude(working_statement))
+
+	# Part 2 Solution
+
+
